8919e8a6abc65ab3a5cb95326ff931eb83e821e1
Zhang <823253467@qq.com>
2017-11-04 14:44:22 +0800

    12345

---
+++ moni/src/main/java/com/bwei/moni/TitleView.java
@@ -0,0 +1,23 @@
+package com.bwei.moni;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Created by MK on 2017/11/4.
+ */
+public class TitleView extends LinearLayout{
+    public TitleView(Context context) {
+        super(context);
+    }
+
+    public TitleView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+    }
+
+    public TitleView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
